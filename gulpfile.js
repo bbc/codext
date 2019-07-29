3c4f8e897127620af3c7e50c64169f8d210eb27d
PyvesB <pierre-yves.bigourdan@bbc.co.uk>
2019-07-29 14:41:58 +0100

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const minify = require("gulp-minify");
 const open = require("gulp-open");
 const os = require("os");
 const zip = require("gulp-zip");
-const runSequence = require("run-sequence");
 
 const paths = {
     plugin_sources: [ "background/**/*", "content/**/*", "editor/**/*", "images/**/*", "manifest.json" ],
@@ -18,23 +17,17 @@ const paths = {
         "node_modules/sinon-chrome/bundle/sinon-chrome.min.js" ]
 };
 
-gulp.task("clean", function(callback) {
-    runSequence("clean-test", "clean-build", callback);
-});
-
-gulp.task("clean-test", function() {
+gulp.task("clean-test", gulp.series(function() {
     return del([ "tests/**/*", "!tests/spec/**", "!tests/SpecRunner.html" ]);
-});
+}));
 
-gulp.task("clean-build", function() {
+gulp.task("clean-build", gulp.series(function() {
     return del([ "build/**/*" ]);
-});
+}));
 
-gulp.task("build", function(callback) {
-    runSequence("clean-build", "minify-sources", "include-minified-modules", "produce-zip", callback);
-});
+gulp.task("clean", gulp.series("clean-test", "clean-build"));
 
-gulp.task("minify-sources", function() {
+gulp.task("minify-sources", gulp.series(function() {
     return gulp.src(paths.plugin_sources, {
         base: "."
     }).pipe(minify({
@@ -43,9 +36,9 @@ gulp.task("minify-sources", function() {
             min: ".js"
         }
     })).pipe(gulp.dest("build"));
-});
+}));
 
-gulp.task("include-minified-modules", function() {
+gulp.task("include-minified-modules", gulp.series(function() {
     return gulp.src(paths.included_modules, {
         base: "node_modules"
     }).pipe(minify({
@@ -56,57 +49,53 @@ gulp.task("include-minified-modules", function() {
         exclude: [ "monaco-editor" ], // Already minified.
         preserveComments: "some" // Preserve licensing information in require.js.
     })).pipe(gulp.dest("build/lib"));
-});
+}));
 
-gulp.task("produce-zip", function() {
+gulp.task("produce-zip", gulp.series(function() {
     return gulp.src("build/**/*").pipe(zip("codext.zip")).pipe(gulp.dest("build"));
-});
+}));
 
-gulp.task("test", function(callback) {
-    runSequence("clean-test", "copy-test-modules", "run-tests-chrome", callback);
-});
-
-gulp.task("test-firefox", function(callback) {
-    runSequence("clean", "copy-test-modules", "run-tests-firefox", callback);
-});
-
-gulp.task("copy-test-modules", function() {
+gulp.task("copy-test-modules", gulp.series(function() {
     return gulp.src(paths.test_modules, {
         base: "node_modules"
     }).pipe(gulp.dest("tests"));
-});
+}));
 
-gulp.task("run-tests-chrome", function() {
+gulp.task("run-tests-chrome", gulp.series(function() {
     const browser = os.platform() === "linux" ? "google-chrome"
         : (os.platform() === "darwin" ? "google chrome" : "chrome");
     return gulp.src("./tests/SpecRunner.html").pipe(open({
         app: browser
     }));
-});
+}));
 
-gulp.task("run-tests-firefox", function() {
+gulp.task("run-tests-firefox", gulp.series(function() {
     return gulp.src("./tests/SpecRunner.html").pipe(open({
         app: "firefox"
     }));
-});
+}));
 
-gulp.task("lint", function(callback) {
-    runSequence("lint-src", "lint-test", callback);
-});
+gulp.task("test", gulp.series("clean-test", "copy-test-modules", "run-tests-chrome"));
 
-gulp.task("lint-src", function() {
+gulp.task("test-firefox", gulp.series("clean", "copy-test-modules", "run-tests-firefox"));
+
+gulp.task("build", gulp.series("clean-build", "minify-sources", "include-minified-modules", "produce-zip"));
+
+gulp.task("lint-src", gulp.series(function() {
     return gulp.src(["**/*.js","!build/**","!node_module/**","!tests/**"])
         .pipe(eslint(".eslintrc.json"))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-});
+}));
 
-gulp.task("lint-test", function() {
+gulp.task("lint-test", gulp.series(function() {
     return gulp.src(["tests/**.js","!tests/sinon-chrome/**","!tests/jasmine-core/**"])
         .pipe(eslint("tests/.eslintrc.json"))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-});
+}));
+
+gulp.task("lint", gulp.series("lint-src", "lint-test"));
 
 // The default task (called when you run `gulp` from cli)
-gulp.task("default", [ "build" ]);
+gulp.task("default", gulp.series("build"));
